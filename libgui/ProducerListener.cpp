298ee0bd87dc587fbe77eed7b86e8bf9d0a35cb2
Omkar Chandorkar <gotenksIN@aospa.co>
2026-07-13 10:33:19 +0530

    libgui: Add shim for xiaomi libgui
    Change-Id: I239cc8400659e925d65a0325485c99333d54f95e
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

---
+++ libgui/ProducerListener.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2026 Paranoid Android
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+
+extern "C" void _ZN7android18BnProducerListener16onBufferDetachedEim(void* /*thisptr*/,
+                                                                      int /*slot*/,
+                                                                      uint64_t /*bufferId*/) {}
